f021db6574382e49c95291f158212b928da60d24
Kaushal Kumar <116164546+Kaushal14200@users.noreply.github.com>
2023-01-31 09:52:04 +0530

    Update Divisible Sum Pairs.cpp

--- Assignment - 4/Divisible Sum Pairs.cpp
+++ Assignment - 4/Divisible Sum Pairs.cpp
@@ -5,21 +5,7 @@
 #include <ctime>
 #include <deque>
 #include <queue>
-#include <stack>
-#include <string>
-#include <bitset>
-#include <cstdio>
-#include <limits>
-#include <vector>
-#include <climits>
-#include <cstring>
-#include <cstdlib>
-#include <fstream>
-#include <numeric>
-#include <sstream>
-#include <iostream>
-#include <algorithm>
-#include <unordered_map>
+
 
 using namespace std;
 
